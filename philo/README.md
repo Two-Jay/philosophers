3474c1873900017896b43f844cf4c556bc3b218e
Two-Jay <djeeee1272@gmail.com>
2021-07-29 13:22:56 +0900

    added readme and basic sturcts on header

---
+++ philo/README.md
@@ -0,0 +1,9 @@
+## philosophers - 42
+
+this project is for an implementation of Dining-philosopher problem in c.
+
+>
+> ./ make
+> ./philo [number_of_philosophers time_to_die] [time_to_eat] [time_to_sleep] [number_of_times_each_philosopher_must_eat]
+
+`number_of_times_each_philosopher_must_eat` is optional.
--- philo/philo.h
+++ philo/philo.h
@@ -6,13 +6,37 @@
 /*   By: jekim <arabi1549@naver.com>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/29 05:04:37 by jekim             #+#    #+#             */
-/*   Updated: 2021/07/29 05:07:30 by jekim            ###   ########seoul.kr  */
+/*   Updated: 2021/07/29 13:17:55 by jekim            ###   ########seoul.kr  */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PHILO_H
+# define PHILO_H
 
 # include <stdio.h>
 # include "../libft/libft.h"
+
+typedef struct s_philo
+{
+	int	state;
+	int	L_fork;
+	int	R_fork;
+}				t_philo;
+
+typedef struct s_fork
+{
+	int	Ownedby;
+}				t_fork;
+
+typedef struct s_data
+{
+	struct s_philo	*philo_arr;
+	struct s_fork	*fork_arr;
+	int				number_philo;
+	int				number_must_eat;
+	int				T_eat;
+	int				T_sleep;
+	int				T_die;
+}				t_data;
 
 #endif
