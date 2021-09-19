644244fc4da296f822523d7cdcc09eb3ada43705
Two-Jay <djeeee1272@gmail.com>
2021-09-19 17:40:01 +0900

    fn_gettimenow refactoring comp

--- philo/utils.c
+++ philo/utils.c
@@ -6,7 +6,7 @@
 /*   By: jekim <arabi1549@naver.com>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/17 02:20:43 by jekim             #+#    #+#             */
-/*   Updated: 2021/09/19 17:39:23 by jekim            ###   ########seoul.kr  */
+/*   Updated: 2021/09/19 17:39:56 by jekim            ###   ########seoul.kr  */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,8 +33,9 @@ unsigned long	fn_gettimenow(t_data *data)
 {
 	struct timeval	now_tv;
 	struct timeval	start_tv;
-	
+
 	start_tv = data->time_to_start_tv;
 	gettimeofday(&now_tv, NULL);
-	return (((now_tv.tv_sec - start_tv.tv_sec) * 1000) + ((now_tv.tv_usec - start_tv.tv_usec) / 1000));
+	return (((now_tv.tv_sec - start_tv.tv_sec) * 1000)
+		+ ((now_tv.tv_usec - start_tv.tv_usec) / 1000));
 }
