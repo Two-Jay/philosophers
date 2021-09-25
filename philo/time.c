30db122f6921c592e9f33538bbca1239849514e5
djeeee1272@gmail.com <djeeee1272@gmail.com>
2021-09-25 17:33:02 +0900

    submission fin

--- philo/time.c
+++ philo/time.c
@@ -6,7 +6,7 @@
 /*   By: jekim <arabi1549@naver.com>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/17 02:20:43 by jekim             #+#    #+#             */
-/*   Updated: 2021/09/25 17:17:28 by jekim            ###   ########.fr       */
+/*   Updated: 2021/09/25 17:32:30 by jekim            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -68,5 +68,33 @@ int	get_sleep(unsigned long sleep_time, t_data *data, t_philo *philo)
 		if (check_philo_health(philo, 0))
 			return (1);
 	}
+	return (0);
+}
+
+int	print_messsage_stdout(t_philo *philo)
+{
+	char *msg;
+
+	if (philo->state == FORK)
+		msg = "has taken a fork.\n";
+	if (philo->state == DFORK)
+		msg = "has dropped a fork.\n";
+	if (philo->state == EAT)
+		msg = "is eating.\n";
+	if (philo->state == SLEEP)
+		msg = "is sleeping.\n";
+	if (philo->state == THINK)
+		msg = "is thinking.\n";
+	if (philo->state == END)
+		msg = "was eaten as the philo must eat.\n";
+	if (philo->state == DIE)
+		msg = "died.\n";
+	pthread_mutex_lock(&(philo->data->isAnyoneDead_mtx));
+	printf("%lu ms %dth philo %s",
+		time_from_start(philo->data),
+		philo->id,
+		msg);
+	if (philo->data->isAnyoneDead == 0)
+		pthread_mutex_unlock(&(philo->data->isAnyoneDead_mtx));
 	return (0);
 }
