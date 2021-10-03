57db3de85672437fba6031c177bb8b5e5636c885
Two-Jay <djeeee1272@gmail.com>
2021-10-04 02:34:37 +0900

    edited params in main functions

--- philo_bonus/philo_bonus.c
+++ philo_bonus/philo_bonus.c
@@ -6,7 +6,7 @@
 /*   By: jekim <arabi1549@naver.com>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/03 07:04:42 by jekim             #+#    #+#             */
-/*   Updated: 2021/10/04 02:32:38 by jekim            ###   ########seoul.kr  */
+/*   Updated: 2021/10/04 02:34:12 by jekim            ###   ########seoul.kr  */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,11 +21,11 @@ int	assign_setting(t_setting *set, int argc, char **argv)
 	return (0);
 }
 
-int	main(void)
+int	main(int argc, char **argv)
 {
 	t_setting	set;
 
-	if (assign_setting(&set))
+	if (assign_setting(&set, argc, argv))
 		ft_strerr("Error\n");
 	return (0);
 }
